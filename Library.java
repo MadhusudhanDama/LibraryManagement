83c12d9cb54f011d408f9c08582ef3c5c9418d99
MadhusudhanDama <52232163+MadhusudhanDama@users.noreply.github.com>
2019-06-26 11:49:41 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,33 @@
+package com.task1;
+public class Library {
+	 int bId;
+     String bName;
+	 String bAuthor;
+	 String bPublisher;
+	public int getbId() {
+		return bId;
+	}
+	public void setbId(int bId) {
+		this.bId = bId;
+	}
+	public String getbName() {
+		return bName;
+	}
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+	
+	public String getbAuthor() {
+		return bAuthor;
+	}
+	public void setbAuthor(String bAuthor)
+	{
+		this.bAuthor=bAuthor;
+	}
+	public void setbPublisher(String bPublisher) {
+		this.bPublisher = bPublisher;
+	}
+	public String getbPublisher() {
+		return bPublisher;
+	}
+}
